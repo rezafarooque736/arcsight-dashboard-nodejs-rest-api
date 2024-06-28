66d343086d002ac26f748f772a488560ff5db0d2
Farooque Reza <rezafarooque@outlook.com>
2024-06-28 11:59:03 +0530

    refactor: update errorHandler middleware

--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -7,7 +7,6 @@ class ApiError extends Error {
   ) {
     super(message);
     this.statusCode = statusCode;
-    this.data = null;
     this.message = message;
     this.success = false;
     this.errors = errors;
@@ -18,6 +17,30 @@ class ApiError extends Error {
       Error.captureStackTrace(this, this.constructor);
     }
   }
+
+  toApiResponse() {
+    return {
+      statusCode: this.statusCode,
+      message: this.message,
+      success: this.success,
+      errors: this.errors,
+    };
+  }
 }
 
 export default ApiError;
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof ApiError) {
+    const apiResponse = err.toApiResponse();
+    return res.status(apiResponse.statusCode).json(apiResponse);
+  }
+
+  // Handle other types of errors (e.g., 500 Internal Server Error)
+  return res.status(500).json({
+    statusCode: 500,
+    message: "Internal Server Error",
+    success: false,
+    errors: [err.message],
+  });
+};
